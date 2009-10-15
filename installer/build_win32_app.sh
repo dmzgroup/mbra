5cd730340af28772b0fdc322e376632fd9b68c24
Randall E. Barker <simstorm@mac.com>
2009-10-15 14:35:48 -0700

    win32 installer update

--- installer/build_win32_app.sh
+++ installer/build_win32_app.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+DEPTH=../../..
 rm -f ./mbrasetup.exe
 lmk -m opt -b
-../../../depend/InnoSetup5/ISCC.exe mbra.iss
+$DEPTH/depend/InnoSetup5/ISCC.exe mbra.iss
+cp mbrasetup.exe $DEPTH/MBRA-`cat $DEPTH/tmp/macos-opt/mbraapp/buildnumber.txt`.exe
