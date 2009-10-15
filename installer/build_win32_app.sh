0dc16fec4c5e948f991faa984128128839d17fbc
Randall E. Barker <simstorm@mac.com>
2009-10-15 14:48:57 -0700

    fixed typo in MBRA build script

--- installer/build_win32_app.sh
+++ installer/build_win32_app.sh
@@ -3,4 +3,4 @@ DEPTH=../../..
 rm -f ./mbrasetup.exe
 lmk -m opt -b
 $DEPTH/depend/InnoSetup5/ISCC.exe mbra.iss
-cp mbrasetup.exe $DEPTH/MBRA-`cat $DEPTH/tmp/macos-opt/mbraapp/buildnumber.txt`.exe
+cp mbrasetup.exe $DEPTH/MBRA-`cat $DEPTH/tmp/win32-opt/mbraapp/buildnumber.txt`.exe
