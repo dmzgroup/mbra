1cbcf3cde45a17bb964d4f409183d08c5a797109
Scott Shillcock <scott@shillcock.net>
2009-10-15 18:44:15 -0700

    update publish to scp the two files to new update.dmzdev.org server

--- installer/publish_mac_app.sh
+++ installer/publish_mac_app.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+
 DEPTH=../../..
-cp $DEPTH/bin/macos-opt/MBRA.app/Contents/Resources/config/version.xml $DEPTH/swupdate/dmz/latest/stable/macos/MBRA.xml
-cp $DEPTH/MBRA-`cat $DEPTH/tmp/macos-opt/mbraapp/buildnumber.txt`.dmg $DEPTH/swupdate/dmz/downloads
-$DEPTH/swupdate/google_appengine/appcfg.py update $DEPTH/swupdate/dmz
+
+scp $DEPTH/bin/macos-opt/MBRA.app/Contents/Resources/config/version.xml dmzdev.org:/home/update/public/latest/stable/macos/MBRA.xml
+
+scp $DEPTH/installers/MBRA-`cat $DEPTH/tmp/macos-opt/mbraapp/buildnumber.txt`.dmg dmzdev.org:/home/update/public/downloads
+
