16660be3ef42f477a936dcea0534ec798daa169c
Scott Shillcock <scott@shillcock.net>
2009-10-20 15:00:54 -0700

    updated publish script

--- installer/publish_mac_app.sh
+++ installer/publish_mac_app.sh
@@ -2,7 +2,21 @@
 
 DEPTH=../../..
 
-scp $DEPTH/bin/macos-opt/MBRA.app/Contents/Resources/config/version.xml dmzdev.org:/home/update/public/latest/stable/macos/MBRA.xml
+CHANNEL=$1
 
-scp $DEPTH/installers/MBRA-`cat $DEPTH/tmp/macos-opt/mbraapp/buildnumber.txt`.dmg dmzdev.org:/home/update/public/downloads
+if [ "$CHANNEL" = "" ] ; then
+   CHANNEL=devel
+fi
 
+VERSION_XML=$DEPTH/bin/macos-opt/MBRA.app/Contents/Resources/config/version.xml
+INSTALLER=$DEPTH/installers/MBRA-`cat $DEPTH/tmp/macos-opt/mbraapp/buildnumber.txt`.dmg
+
+echo "publishing $INSTALLER..."
+
+echo "scp $VERSION_XML dmzdev.org:/home/update/public/latest/$CHANNEL/macos/MBRA.xml"
+scp $VERSION_XML dmzdev.org:/home/update/public/latest/$CHANNEL/macos/MBRA.xml
+
+echo "scp $INSTALLER dmzdev.org:/home/update/public/downloads"
+scp $INSTALLER dmzdev.org:/home/update/public/downloads
+
+echo "done!"
