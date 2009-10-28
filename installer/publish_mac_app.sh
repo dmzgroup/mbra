b8ef5c1bb8178365188dd5f67f405f4c6df33847
Scott Shillcock <scott@shillcock.net>
2009-10-28 10:37:58 -0700

    updated config to use new app updater preferences tab. updated the publish script. bumped to beta 4

--- installer/publish_mac_app.sh
+++ installer/publish_mac_app.sh
@@ -9,12 +9,16 @@ if [ "$CHANNEL" = "" ] ; then
 fi
 
 VERSION_XML=$DEPTH/bin/macos-opt/MBRA.app/Contents/Resources/config/version.xml
-INSTALLER=$DEPTH/installers/MBRA-`cat $DEPTH/tmp/macos-opt/mbraapp/buildnumber.txt`.dmg
+UPDATE=MBRA-`cat $DEPTH/tmp/macos-opt/mbraapp/buildnumber.txt`
+INSTALLER=$DEPTH/installers/$UPDATE.dmg
 
 echo "publishing $INSTALLER..."
 
-echo "scp $VERSION_XML dmzdev.org:/home/update/public/latest/$CHANNEL/macos/MBRA.xml"
-scp $VERSION_XML dmzdev.org:/home/update/public/latest/$CHANNEL/macos/MBRA.xml
+echo "scp $VERSION_XML dmzdev.org:/home/update/public/latest/macos-$CHANNEL/MBRA.xml"
+scp $VERSION_XML dmzdev.org:/home/update/public/latest/macos-$CHANNEL/MBRA.xml
+
+echo "scp ./changelog.html dmzdev.org:/home/update/public/downloads/$UPDATE.html"
+scp ./changelog.html dmzdev.org:/home/update/public/downloads/$UPDATE.html
 
 echo "scp $INSTALLER dmzdev.org:/home/update/public/downloads"
 scp $INSTALLER dmzdev.org:/home/update/public/downloads
