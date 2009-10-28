8465507d8f4155b2cb159056e5e9a2da6fbb108f
Scott Shillcock <scott@shillcock.net>
2009-10-28 13:49:09 -0700

    updated to fix permissions after upload

--- installer/publish_win32_app.sh
+++ installer/publish_win32_app.sh
@@ -8,14 +8,21 @@ if [ "$CHANNEL" = "" ] ; then
 fi
 
 VERSION_XML=$DEPTH/bin/win32-opt/MBRA.app/config/version.xml
-INSTALLER=$DEPTH/installers/MBRA-`cat $DEPTH/tmp/win32-opt/mbraapp/buildnumber.txt`.exe
+UPDATE=MBRA-`cat $DEPTH/tmp/win32-opt/mbraapp/buildnumber.txt`
+INSTALLER=$DEPTH/installers/$UPDATE.exe
 
 echo "publishing $INSTALLER..."
 
-echo "scp $VERSION_XML dmzdev.org:/home/update/public/latest/$CHANNEL/win32/MBRA.xml"
-scp $VERSION_XML dmzdev.org:/home/update/public/latest/$CHANNEL/win32/MBRA.xml
+echo "scp $VERSION_XML dmzdev.org:/home/update/public/latest/win32-$CHANNEL/MBRA.xml"
+scp $VERSION_XML dmzdev.org:/home/update/public/latest/win32-$CHANNEL/MBRA.xml
+
+echo "scp ./changelog.html dmzdev.org:/home/update/public/downloads/$UPDATE.html"
+scp ./changelog.html dmzdev.org:/home/update/public/downloads/$UPDATE.html
 
 echo "scp $INSTALLER dmzdev.org:/home/update/public/downloads"
 scp $INSTALLER dmzdev.org:/home/update/public/downloads
+
+echo "ssh dmzdev.org sudo chown www-data.admin -R /home/update/public"
+ssh dmzdev.org sudo chown www-data.admin -R /home/update/public
 
 echo "done!"
