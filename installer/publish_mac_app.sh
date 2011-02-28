5007fe1c4a3c8cd25f3e49c91696afe2e761f97b
Benjamin Sangster <benjamin.e.sangster@gmail.com>
2011-02-28 12:59:36 -0800

    Updated publish scripts.

--- installer/publish_mac_app.sh
+++ installer/publish_mac_app.sh
@@ -15,25 +15,25 @@ INSTALLER=$DEPTH/installers/$UPDATE.dmg
 
 echo "publishing $INSTALLER..."
 
-echo "scp $VERSION_XML dmzdev.org:/home/update.dmzdev.org/public/latest/macos-$CHANNEL/MBRA.xml"
-scp $VERSION_XML dmzdev.org:/home/update.dmzdev.org/public/latest/macos-$CHANNEL/MBRA.xml
+echo "scp $VERSION_XML update.dmzdev.org:/home/update.dmzdev.org/public/latest/macos-$CHANNEL/MBRA.xml"
+scp $VERSION_XML update.dmzdev.org:/home/update.dmzdev.org/public/latest/macos-$CHANNEL/MBRA.xml
 
-echo "scp ./changelog.html dmzdev.org:/home/update.dmzdev.org/public/downloads/$UPDATE.html"
-scp ./changelog.html dmzdev.org:/home/update.dmzdev.org/public/downloads/$UPDATE.html
+echo "scp ./changelog.html update.dmzdev.org:/home/update.dmzdev.org/public/downloads/$UPDATE.html"
+scp ./changelog.html update.dmzdev.org:/home/update.dmzdev.org/public/downloads/$UPDATE.html
 
-echo "scp ./changelog.html dmzdev.org:/home/update.dmzdev.org/public/downloads/$OLD_UPDATE.html"
-scp ./changelog.html dmzdev.org:/home/update.dmzdev.org/public/downloads/$OLD_UPDATE.html
+echo "scp ./changelog.html update.dmzdev.org:/home/update.dmzdev.org/public/downloads/$OLD_UPDATE.html"
+scp ./changelog.html update.dmzdev.org:/home/update.dmzdev.org/public/downloads/$OLD_UPDATE.html
 
-echo "scp $INSTALLER dmzdev.org:/home/update.dmzdev.org/public/downloads"
-scp $INSTALLER dmzdev.org:/home/update.dmzdev.org/public/downloads
+echo "scp $INSTALLER update.dmzdev.org:/home/update.dmzdev.org/public/downloads"
+scp $INSTALLER update.dmzdev.org:/home/update.dmzdev.org/public/downloads
 
-echo "ssh dmzdev.org sudo ln -s /home/update.dmzdev.org/public/downloads/$UPDATE.dmg /home/update.dmzdev.org/public/downloads/$OLD_UPDATE.dmg"
-ssh dmzdev.org sudo ln -s /home/update.dmzdev.org/public/downloads/$UPDATE.dmg /home/update.dmzdev.org/public/downloads/$OLD_UPDATE.dmg
+echo "ssh update.dmzdev.org sudo ln -s /home/update.dmzdev.org/public/downloads/$UPDATE.dmg /home/update.dmzdev.org/public/downloads/$OLD_UPDATE.dmg"
+ssh update.dmzdev.org sudo ln -s /home/update.dmzdev.org/public/downloads/$UPDATE.dmg /home/update.dmzdev.org/public/downloads/$OLD_UPDATE.dmg
 
-echo "ssh dmzdev.org sudo chown www-data.admin -R /home/update.dmzdev.org/public"
-ssh dmzdev.org sudo chown www-data.admin -R /home/update.dmzdev.org/public
+echo "ssh update.dmzdev.org sudo chown www-data.admin -R /home/update.dmzdev.org/public"
+ssh update.dmzdev.org sudo chown www-data.admin -R /home/update.dmzdev.org/public
 
-echo "ssh dmzdev.org sudo chmod -R g+w /home/update.dmzdev.org/public"
-ssh dmzdev.org sudo chmod -R g+w /home/update.dmzdev.org/public
+echo "ssh update.dmzdev.org sudo chmod -R g+w /home/update.dmzdev.org/public"
+ssh update.dmzdev.org sudo chmod -R g+w /home/update.dmzdev.org/public
 
 echo "done!"
--- installer/publish_win32_app.sh
+++ installer/publish_win32_app.sh
@@ -15,24 +15,24 @@ INSTALLER=$DEPTH/installers/$UPDATE.exe
 echo "publishing $INSTALLER..."
 
 echo "scp $VERSION_XML dmzdev.org:/home/update.dmzdev.org/public/latest/win32-$CHANNEL/MBRA.xml"
-scp $VERSION_XML dmzdev.org:/home/update.dmzdev.org/public/latest/win32-$CHANNEL/MBRA.xml
+scp $VERSION_XML update.dmzdev.org:/home/update.dmzdev.org/public/latest/win32-$CHANNEL/MBRA.xml
 
-echo "scp ./changelog.html dmzdev.org:/home/update.dmzdev.org/public/downloads/$UPDATE.html"
-scp ./changelog.html dmzdev.org:/home/update.dmzdev.org/public/downloads/$UPDATE.html
+echo "scp ./changelog.html update.dmzdev.org:/home/update.dmzdev.org/public/downloads/$UPDATE.html"
+scp ./changelog.html update.dmzdev.org:/home/update.dmzdev.org/public/downloads/$UPDATE.html
 
-echo "scp ./changelog.html dmzdev.org:/home/update.dmzdev.org/public/downloads/$OLD_UPDATE.html"
-scp ./changelog.html dmzdev.org:/home/update.dmzdev.org/public/downloads/$OLD_UPDATE.html
+echo "scp ./changelog.html update.dmzdev.org:/home/update.dmzdev.org/public/downloads/$OLD_UPDATE.html"
+scp ./changelog.html update.dmzdev.org:/home/update.dmzdev.org/public/downloads/$OLD_UPDATE.html
 
-echo "scp $INSTALLER dmzdev.org:/home/update.dmzdev.org/public/downloads"
-scp $INSTALLER dmzdev.org:/home/update.dmzdev.org/public/downloads
+echo "scp $INSTALLER update.dmzdev.org:/home/update.dmzdev.org/public/downloads"
+scp $INSTALLER update.dmzdev.org:/home/update.dmzdev.org/public/downloads
 
-echo "ssh dmzdev.org sudo ln -s /home/update.dmzdev.org/public/downloads/$UPDATE.exe /home/update.dmzdev.org/public/downloads/$OLD_UPDATE.exe"
-ssh dmzdev.org sudo ln -s /home/update.dmzdev.org/public/downloads/$UPDATE.exe /home/update.dmzdev.org/public/downloads/$OLD_UPDATE.exe
+echo "ssh update.dmzdev.org sudo ln -s /home/update.dmzdev.org/public/downloads/$UPDATE.exe /home/update.dmzdev.org/public/downloads/$OLD_UPDATE.exe"
+ssh update.dmzdev.org sudo ln -s /home/update.dmzdev.org/public/downloads/$UPDATE.exe /home/update.dmzdev.org/public/downloads/$OLD_UPDATE.exe
 
-echo "ssh dmzdev.org sudo chown www-data.admin -R /home/update.dmzdev.org/public"
-ssh dmzdev.org sudo chown www-data.admin -R /home/update.dmzdev.org/public
+echo "ssh update.dmzdev.org sudo chown www-data.admin -R /home/update.dmzdev.org/public"
+ssh update.dmzdev.org sudo chown www-data.admin -R /home/update.dmzdev.org/public
 
-echo "ssh dmzdev.org sudo chmod -R g+w /home/update.dmzdev.org/public"
-ssh dmzdev.org sudo chmod -R g+w /home/update.dmzdev.org/public
+echo "ssh update.dmzdev.org sudo chmod -R g+w /home/update.dmzdev.org/public"
+ssh update.dmzdev.org sudo chmod -R g+w /home/update.dmzdev.org/public
 
 echo "done!"
